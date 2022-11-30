452184409d6e7927ac1e018f5fbcc3503596758a
Larisa-Romero <119458932+Larisa-Romero@users.noreply.github.com>
2022-11-30 00:03:31 -0300

    Add files via upload

---
+++ 28-constructing-strings-with-variables.js
@@ -0,0 +1,4 @@
+// Only change code below this line
+const myName = "Larisa";
+const myStr = "Mi nombre es" + myName + "y estoy bien";
+
---
+++ 7-understanding-case-sensitivity-in-variables.js
@@ -0,0 +1,10 @@
+// Variable declarations
+var StUdLyCapVaR;
+var studlyCapVar;
+studlyCapVar = 10;
+var properCamelCase;
+properCamelCase = "A String"
+var TitleCaseOver;
+var titleCaseOver;
+titleCaseOver = 9000;
+
